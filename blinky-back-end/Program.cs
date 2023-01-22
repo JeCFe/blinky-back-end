a43b1743e7aed05eb24533aa38cf8a6da3da145a
Jessica Fealy <jessicaf091@outlook.com>
2023-01-22 12:53:49 +0000

    Getting swagger to work
    Spike to get example api contract set up

--- blinky-back-end/Program.cs
+++ blinky-back-end/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new() {Title="Spike Api", Version="v1"});});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/test", () => "Testing");
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint(
+    "/swagger/v1/swagger.json", "v1"
+));
 
 app.Run();
