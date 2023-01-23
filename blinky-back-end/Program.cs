ac045d958282318fba078adaba76b8868cc56cd8
Jessica Fealy <jessicaf091@outlook.com>
2023-01-23 13:46:13 +0000

    Handled cors

--- blinky-back-end/Program.cs
+++ blinky-back-end/Program.cs
@@ -1,5 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: "PolicyOne", 
+    policy => {
+        policy.AllowAnyOrigin();
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => {
     c.SwaggerDoc("v1", new() {Title="Spike Api", Version="v1"});});
@@ -8,6 +15,8 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/test", () => "Testing");
+
+app.UseCors("PolicyOne");
 
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint(
